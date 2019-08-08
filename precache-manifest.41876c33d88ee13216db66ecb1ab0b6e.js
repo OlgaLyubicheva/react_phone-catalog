d95d7fa253c1f0d1b5a753236e803ad0c06c600a
Olga Lyubicheva <forjobslov@gmail.com>
2019-08-08 10:40:59 +0300

    Updates

---
+++ precache-manifest.41876c33d88ee13216db66ecb1ab0b6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e757c8934841ee66ddb8f72f5ec6259",
+    "url": "/react_phone-catalog/index.html"
+  },
+  {
+    "revision": "ede4c4ca8cb37b00aa38",
+    "url": "/react_phone-catalog/static/css/main.546b3980.chunk.css"
+  },
+  {
+    "revision": "3cfc1a7936fe3b90944a",
+    "url": "/react_phone-catalog/static/js/2.123b833f.chunk.js"
+  },
+  {
+    "revision": "ede4c4ca8cb37b00aa38",
+    "url": "/react_phone-catalog/static/js/main.53563643.chunk.js"
+  },
+  {
+    "revision": "9b3e1d68b9cc1dd9a925",
+    "url": "/react_phone-catalog/static/js/runtime~main.25bc40f7.js"
+  }
+]);
